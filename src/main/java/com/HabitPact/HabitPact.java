da14a3b813bbb55711b5255faccefac356819bee
sanjay123-321 <sanjayup21@gmail.com>
2026-09-16 00:07:29 -0400

    First Commit

---
+++ src/main/java/com/HabitPact/HabitPact.java
@@ -0,0 +1,13 @@
+package com.HabitPact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitPact {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitPact.class, args);
+	}
+
+}
---
+++ src/test/java/com/HabitPact/HabitPactTests.java
@@ -0,0 +1,13 @@
+package com.HabitPact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitPactTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
