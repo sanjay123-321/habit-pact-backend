ce7c9f9b2f2354af49e2b45726ccd1ef8cdeeacd
sanjay123-321 <sanjayup21@gmail.com>
2026-09-19 12:48:34 -0400

    entity code for pact and user added

--- src/main/java/com/HabitPact/entity/Pact.java
+++ src/main/java/com/HabitPact/entity/Pact.java
@@ -1,4 +1,30 @@
 package com.HabitPact.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
 public class Pact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "receiver_id",nullable = false)
+    private User receiver;
+
+    private LocalDate senderLastCheckin;
+    private LocalDate receiverLastCheckin;
+
+    private int streakCount;
+    private String habitName;
+
+    private LocalDate startDate;
 }
--- src/main/java/com/HabitPact/entity/User.java
+++ src/main/java/com/HabitPact/entity/User.java
@@ -11,7 +11,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long user_id;
 
     @Column(unique = true)
     private String username;
